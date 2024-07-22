074e76b8ab6eb0ef9f3e5342ce19343f503b476e
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-22 13:26:10 +0530

    added heading and a container

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -1,8 +1,14 @@
-import { Flex } from "@chakra-ui/react";
+import { Container, Flex, Heading } from "@chakra-ui/react";
 import React from "react";
 
 function TopNav() {
-  return <Flex boxShadow={"xl"}></Flex>;
+  return (
+    <Flex boxShadow={"xl"}>
+      <Container>
+        <Heading>Dashboard</Heading>
+      </Container>
+    </Flex>
+  );
 }
 
 export default TopNav;
