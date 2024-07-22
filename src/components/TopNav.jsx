8e17ff0a20fff0267e003d154c41a213aa217591
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-22 13:29:44 +0530

    added menu

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -1,4 +1,13 @@
-import { Container, Flex, Heading } from "@chakra-ui/react";
+import {
+  Button,
+  Container,
+  Flex,
+  Heading,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+} from "@chakra-ui/react";
 import React from "react";
 
 function TopNav() {
@@ -6,6 +15,16 @@ function TopNav() {
     <Flex boxShadow={"xl"}>
       <Container>
         <Heading>Dashboard</Heading>
+        <Menu>
+          <MenuButton as={Button}>Actions</MenuButton>
+          <MenuList>
+            <MenuItem>Download</MenuItem>
+            <MenuItem>Create a Copy</MenuItem>
+            <MenuItem>Mark as Draft</MenuItem>
+            <MenuItem>Delete</MenuItem>
+            <MenuItem>Attend a Workshop</MenuItem>
+          </MenuList>
+        </Menu>
       </Container>
     </Flex>
   );
