f3e5f0528cb42ef787b606e7a57ad3d442ce654f
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-22 13:17:56 +0530

    added support and applied justify space between

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -2,6 +2,7 @@ import { Box, Heading, HStack, Icon, Stack, Text } from "@chakra-ui/react";
 import React from "react";
 import { MdDashboard } from "react-icons/md";
 import { TbArrowsExchange } from "react-icons/tb";
+import { BiSupport } from "react-icons/bi";
 
 function Sidenav() {
   const navLinks = [
@@ -18,31 +19,57 @@ function Sidenav() {
   ];
 
   return (
-    <Stack boxShadow="md" maxWidth={"16rem"} height={"100vh"}>
-      <Heading as="h1" fontSize={"20px"} pt={"56px"} textAlign={"center"}>
-        CRYPTO DASHBOARD
-      </Heading>
+    <Stack
+      boxShadow="md"
+      maxWidth={"16rem"}
+      height={"100vh"}
+      justify={"space-between"}
+    >
+      <Box>
+        <Heading as="h1" fontSize={"20px"} pt={"56px"} textAlign={"center"}>
+          CRYPTO DASHBOARD
+        </Heading>
+        <Box mt={"6"} mx={"3"}>
+          {navLinks.map((nav, index) => (
+            <HStack
+              key={index}
+              py={"3"}
+              px={"4"}
+              gap={"4"}
+              color={"#797e82"}
+              borderRadius={"10px"}
+              _hover={{
+                bg: "#f3f3f7",
+                color: "#171717",
+                cursor: "pointer",
+              }}
+            >
+              <Icon as={nav.icon}></Icon>
+              <Text fontSize={"14px"} fontWeight={"bold"}>
+                {nav.text}
+              </Text>
+            </HStack>
+          ))}
+        </Box>
+      </Box>
       <Box mt={"6"} mx={"3"}>
-        {navLinks.map((nav, index) => (
-          <HStack
-            key={index}
-            py={"3"}
-            px={"4"}
-            gap={"4"}
-            color={"#797e82"}
-            borderRadius={"10px"}
-            _hover={{
-              bg: "#f3f3f7",
-              color: "#171717",
-              cursor: "pointer",
-            }}
-          >
-            <Icon as={nav.icon}></Icon>
-            <Text fontSize={"14px"} fontWeight={"bold"}>
-              {nav.text}
-            </Text>
-          </HStack>
-        ))}
+        <HStack
+          py={"3"}
+          px={"4"}
+          gap={"4"}
+          color={"#797e82"}
+          borderRadius={"10px"}
+          _hover={{
+            bg: "#f3f3f7",
+            color: "#171717",
+            cursor: "pointer",
+          }}
+        >
+          <Icon as={BiSupport}></Icon>
+          <Text fontSize={"14px"} fontWeight={"bold"}>
+            Support
+          </Text>
+        </HStack>
       </Box>
     </Stack>
   );
