5549cb75fa076a126483daf064a7e837910c3e36
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-22 10:56:55 +0530

    styled sidenav

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,4 +1,4 @@
-import { HStack, Icon, Stack, Text } from "@chakra-ui/react";
+import { Box, Heading, HStack, Icon, Stack, Text } from "@chakra-ui/react";
 import React from "react";
 import { MdDashboard } from "react-icons/md";
 import { TbArrowsExchange } from "react-icons/tb";
@@ -18,13 +18,32 @@ function Sidenav() {
   ];
 
   return (
-    <Stack>
-      {navLinks.map((nav, index) => (
-        <HStack key={index}>
-          <Icon as={nav.icon}></Icon>
-          <Text>{nav.text}</Text>
-        </HStack>
-      ))}
+    <Stack boxShadow="md" maxWidth={"16rem"} height={"100vh"}>
+      <Heading as="h1" fontSize={"20px"} pt={"56px"} textAlign={"center"}>
+        CRYPTO DASHBOARD
+      </Heading>
+      <Box mt={"6"} mx={"3"}>
+        {navLinks.map((nav, index) => (
+          <HStack
+            key={index}
+            py={"3"}
+            px={"4"}
+            gap={"4"}
+            color={"#797e82"}
+            borderRadius={"10px"}
+            _hover={{
+              bg: "#f3f3f7",
+              color: "#171717",
+              cursor: "pointer",
+            }}
+          >
+            <Icon as={nav.icon}></Icon>
+            <Text fontSize={"14px"} fontWeight={"bold"}>
+              {nav.text}
+            </Text>
+          </HStack>
+        ))}
+      </Box>
     </Stack>
   );
 }
