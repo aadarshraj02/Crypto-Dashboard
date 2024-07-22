85af64d9bf07fe028087f747ab60574938fb5d2c
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-22 13:33:05 +0530

    applied flex grow

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Sidenav from "../../components/Sidenav";
 import TopNav from "../../components/TopNav";
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 
 function Dashboard() {
   return (
     <div>
       <Flex>
-        <Sidenav /> <TopNav />
+        <Sidenav />
+        <Box flexGrow={1}>
+          <TopNav />
+        </Box>
       </Flex>
     </div>
   );
