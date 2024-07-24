bc3c14e054ad1ef232a2ed9bbd96192da388670a
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-24 09:58:33 +0530

    added a hstack for btc

--- src/pages/Dashboard/components/PortfolioSection.jsx
+++ src/pages/Dashboard/components/PortfolioSection.jsx
@@ -1,4 +1,4 @@
-import { HStack, Icon, Stack, Text } from "@chakra-ui/react";
+import { HStack, Icon, Stack, Tag, Text } from "@chakra-ui/react";
 import { BsInfo } from "react-icons/bs";
 import React from "react";
 
@@ -11,6 +11,14 @@ function PortfolioSection() {
           <Icon as={BsInfo}></Icon>
         </HStack>
         <Text textStyle={"h2"}>$112,312.24</Text>
+      </Stack>
+      <Stack>
+        <HStack color={"black.80"}>
+          <Text fontSize={"14px"}>Wallet Balance</Text>
+        </HStack>
+        <HStack>
+          <Text textStyle={"h2"}>22.39401004</Text> <Tag>BTC</Tag>
+        </HStack>
       </Stack>
     </HStack>
   );
