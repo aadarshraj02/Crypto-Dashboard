0e3779bbc4acf411838eebd6b09d257646e90382
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-24 10:08:34 +0530

    wrapped in a hstack and applied justify between; added two buttons for deposit and withdraw

--- src/pages/Dashboard/components/PortfolioSection.jsx
+++ src/pages/Dashboard/components/PortfolioSection.jsx
@@ -1,30 +1,42 @@
-import { HStack, Icon, Stack, Tag, Text } from "@chakra-ui/react";
+import { Button, HStack, Icon, Stack, Tag, Text } from "@chakra-ui/react";
 import { BsInfo } from "react-icons/bs";
 import React from "react";
 
 function PortfolioSection() {
   return (
-    <HStack bg={"white"} borderRadius={"xl"} p={"6"} spacing={"16"}>
-      <Stack>
-        <HStack color={"black.80"}>
-          <Text fontSize={"14px"}>Total Portfolio Value</Text>
-          <Icon as={BsInfo}></Icon>
-        </HStack>
-        <Text textStyle={"h2"}>$112,312.24</Text>
-      </Stack>
-      <Stack>
-        <HStack color={"black.80"}>
-          <Text fontSize={"14px"}>Wallet Balance</Text>
-        </HStack>
-        <HStack spacing={"6"}>
-          <HStack>
-            <Text textStyle={"h2"}>22.39401004</Text> <Tag>BTC</Tag>
+    <HStack
+      justify={"space-between"}
+      bg={"white"}
+      borderRadius={"xl"}
+      p={"6"}
+      spacing={"16"}
+    >
+      <HStack spacing={16}>
+        <Stack>
+          <HStack color={"black.80"}>
+            <Text fontSize={"14px"}>Total Portfolio Value</Text>
+            <Icon as={BsInfo}></Icon>
           </HStack>
-          <HStack>
-            <Text textStyle={"h2"}>₹ 15,000.00</Text> <Tag>INR</Tag>
+          <Text textStyle={"h2"}>$112,312.24</Text>
+        </Stack>
+        <Stack>
+          <HStack color={"black.80"}>
+            <Text fontSize={"14px"}>Wallet Balance</Text>
           </HStack>
-        </HStack>
-      </Stack>
+          <HStack spacing={"6"}>
+            <HStack>
+              <Text textStyle={"h2"}>22.39401004</Text> <Tag>BTC</Tag>
+            </HStack>
+            <HStack>
+              <Text textStyle={"h2"}>₹ 15,000.00</Text> <Tag>INR</Tag>
+            </HStack>
+          </HStack>
+        </Stack>
+      </HStack>
+      <HStack>
+        <Button>Deposit</Button>
+        <Button>Withdrawal</Button>
+      </HStack>
     </HStack>
   );
 }
