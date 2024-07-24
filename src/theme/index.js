a55f5906585ab7ab4e10c7b3c93a448c4dd5ed66
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-24 08:25:46 +0530

    added colors

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,6 +1,22 @@
 import { extendTheme } from "@chakra-ui/react";
+import { color } from "framer-motion";
 
 export const theme = extendTheme({
+  colors: {
+    p: {
+      orange: "#5f00d9",
+      black: "#171717",
+    },
+    black: {
+      5: "#f3f3f7",
+      10: "#eeeef4",
+      20: "#d8dde2",
+      40: "#babac4",
+      60: "#797e82",
+      80: "#535d66",
+    },
+  },
+
   fonts: {
     heading: "Ubuntu",
     body: "Ubuntu",
