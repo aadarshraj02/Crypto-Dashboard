78e216c1838191fd1aa3e3f7f45fffd5d13da207
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-24 07:46:59 +0530

    added stack and icon

--- src/pages/Dashboard/components/PortfolioSection.jsx
+++ src/pages/Dashboard/components/PortfolioSection.jsx
@@ -1,7 +1,16 @@
+import { HStack, Icon, Stack } from "@chakra-ui/react";
+import { BsInfo } from "react-icons/bs";
 import React from "react";
 
 function PortfolioSection() {
-  return <div></div>;
+  return (
+    <HStack>
+      <Stack>
+        <HStack>Total Portfolio Value</HStack>
+        <Icon as={BsInfo}></Icon>
+      </Stack>
+    </HStack>
+  );
 }
 
 export default PortfolioSection;
