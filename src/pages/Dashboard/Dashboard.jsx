68d096949eb39570196dc425710d5fcf1305bf84
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-24 10:54:28 +0530

    imported price section

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -4,11 +4,13 @@ import TopNav from "../../components/TopNav";
 import { Box, Flex } from "@chakra-ui/react";
 import DashboardLayout from "../../components/DashboardLayout";
 import PortfolioSection from "./components/PortfolioSection";
+import PriceSection from "./components/PriceSection";
 
 function Dashboard() {
   return (
     <DashboardLayout title={"Dashboard"}>
       <PortfolioSection />
+      <PriceSection />
     </DashboardLayout>
   );
 }
