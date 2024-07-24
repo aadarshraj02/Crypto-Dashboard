fd47a9d6e1c0b5305dc8fd51f5c572d441259d76
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-24 11:24:02 +0530

    updated into grid layout

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,16 +1,25 @@
 import React from "react";
-import Sidenav from "../../components/Sidenav";
-import TopNav from "../../components/TopNav";
-import { Box, Flex } from "@chakra-ui/react";
 import DashboardLayout from "../../components/DashboardLayout";
 import PortfolioSection from "./components/PortfolioSection";
 import PriceSection from "./components/PriceSection";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 function Dashboard() {
   return (
     <DashboardLayout title={"Dashboard"}>
-      <PortfolioSection />
-      <PriceSection />
+      <Grid
+        gridTemplateColumns={{
+          base: "repeat(1.1fr)",
+          md: "repeat(2,1fr)",
+        }}
+      >
+        <GridItem colSpan={2}>
+          <PortfolioSection />
+        </GridItem>
+        <GridItem colSpan={1}>
+          <PriceSection />
+        </GridItem>
+      </Grid>
     </DashboardLayout>
   );
 }
