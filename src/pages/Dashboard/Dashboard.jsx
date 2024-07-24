1125717e4eb879325b7a06dd2170b10e47ca5edd
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-24 08:15:28 +0530

    imported portfolio section

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -3,9 +3,14 @@ import Sidenav from "../../components/Sidenav";
 import TopNav from "../../components/TopNav";
 import { Box, Flex } from "@chakra-ui/react";
 import DashboardLayout from "../../components/DashboardLayout";
+import PortfolioSection from "./components/PortfolioSection";
 
 function Dashboard() {
-  return <DashboardLayout title={"Dashboard"}>Hi How are you?</DashboardLayout>;
+  return (
+    <DashboardLayout title={"Dashboard"}>
+      <PortfolioSection />
+    </DashboardLayout>
+  );
 }
 
 export default Dashboard;
