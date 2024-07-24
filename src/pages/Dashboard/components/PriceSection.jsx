9c541db664f7453286f9dab1b1754eee086637e0
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-24 11:11:56 +0530

    wrapped in a stack

--- src/pages/Dashboard/components/PriceSection.jsx
+++ src/pages/Dashboard/components/PriceSection.jsx
@@ -6,37 +6,39 @@ import { GoArrowUpRight } from "react-icons/go";
 function PriceSection() {
   return (
     <CustomCard>
-      <Stack>
-        <HStack color={"black.80"}>
-          <Text fontSize={"14px"}>Wallet Balance</Text>
-        </HStack>
-        <HStack
-          spacing={"6"}
-          align={{
-            base: "flex-start",
-            xl: "center",
-          }}
-          flexDir={{
-            base: "column",
-            sm: "row",
-          }}
-        >
-          <HStack>
-            <Text textStyle={"h2"} fontWeight={"medium"}>
-              22.39401004
-            </Text>
-            <HStack fontWeight={"medium"} color={"green.500"}>
-              <Icon as={GoArrowUpRight}></Icon>
-              <Text fontSize={"sm"} fontWeight={"medium"}>
-                0.04%
+      <HStack>
+        <Stack>
+          <HStack color={"black.80"}>
+            <Text fontSize={"14px"}>Wallet Balance</Text>
+          </HStack>
+          <HStack
+            spacing={"6"}
+            align={{
+              base: "flex-start",
+              xl: "center",
+            }}
+            flexDir={{
+              base: "column",
+              sm: "row",
+            }}
+          >
+            <HStack>
+              <Text textStyle={"h2"} fontWeight={"medium"}>
+                22.39401004
               </Text>
+              <HStack fontWeight={"medium"} color={"green.500"}>
+                <Icon as={GoArrowUpRight}></Icon>
+                <Text fontSize={"sm"} fontWeight={"medium"}>
+                  0.04%
+                </Text>
+              </HStack>
             </HStack>
           </HStack>
+        </Stack>
+        <HStack>
+          <Button>Buy</Button>
+          <Button>Sell</Button>
         </HStack>
-      </Stack>
-      <HStack>
-        <Button>Buy</Button>
-        <Button>Sell</Button>
       </HStack>
     </CustomCard>
   );
