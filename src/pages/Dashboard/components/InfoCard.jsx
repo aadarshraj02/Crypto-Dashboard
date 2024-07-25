b04a69087a65bb0a2038ae3529ea42ebf2be6062
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-25 23:26:33 +0530

    applied text as props

--- src/pages/Dashboard/components/InfoCard.jsx
+++ src/pages/Dashboard/components/InfoCard.jsx
@@ -2,16 +2,15 @@ import React from "react";
 import { CustomCard } from "../../../chakra/CustomCard";
 import { Tag, Text } from "@chakra-ui/react";
 
-function InfoCard({ imgUrl }) {
+function InfoCard({ imgUrl, text }) {
   return (
     <CustomCard bgImage={imgUrl} bgSize={"cover"} bgRepeat={"no-repeat"}>
       <Tag bg="p.purple" color={"white"} borderRadius={"full"}>
         {" "}
         Loan
       </Tag>
-      <Text textStyle={"h5"}>
-        Learn more about Loans - keep your Bitcoin, access it's value without
-        selling it.
+      <Text textStyle={"h5"} mt={4} fontWeight={"bold"}>
+        {text}
       </Text>
     </CustomCard>
   );
