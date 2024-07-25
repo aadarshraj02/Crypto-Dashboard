f6584db9180909f039277c03d0e094d028c66d69
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-25 20:19:25 +0530

    added array of transactions

--- src/pages/Dashboard/components/Transactions.jsx
+++ src/pages/Dashboard/components/Transactions.jsx
@@ -3,6 +3,27 @@ import React from "react";
 import { CustomCard } from "../../../chakra/CustomCard";
 
 function Transactions() {
+  const transactions = [
+    {
+      icon: "",
+      text: "INR Deposit",
+      amount: "₹ 8,00,000.00",
+      timestamp: "2024-07-22 7:05 PM",
+    },
+    {
+      icon: "",
+      text: "INR Deposit",
+      amount: "₹ 8,00,000.00",
+      timestamp: "2024-07-22 7:05 PM",
+    },
+    {
+      icon: "",
+      text: "INR Deposit",
+      amount: "₹ 8,00,000.00",
+      timestamp: "2024-07-22 7:05 PM",
+    },
+  ];
+
   return (
     <CustomCard>
       <Text fontSize={"sm"} color={"black.80"}>
