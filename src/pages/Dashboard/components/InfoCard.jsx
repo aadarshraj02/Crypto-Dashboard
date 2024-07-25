54d16f157133e721ebb39453ab70bdc6d2740433
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-25 23:40:19 +0530

    ic to display color of font

--- src/pages/Dashboard/components/InfoCard.jsx
+++ src/pages/Dashboard/components/InfoCard.jsx
@@ -4,7 +4,12 @@ import { Tag, Text } from "@chakra-ui/react";
 
 function InfoCard({ imgUrl, text, tagText, inverted }) {
   return (
-    <CustomCard bgImage={imgUrl} bgSize={"cover"} bgRepeat={"no-repeat"}>
+    <CustomCard
+      bgImage={imgUrl}
+      bgSize={"cover"}
+      bgRepeat={"no-repeat"}
+      bgColor={inverted ? "p.purple" : "white"}
+    >
       <Tag
         color={inverted ? "p.purple" : "white"}
         bg={inverted ? "white" : "p.purple"}
@@ -13,7 +18,12 @@ function InfoCard({ imgUrl, text, tagText, inverted }) {
         {" "}
         {tagText}
       </Tag>
-      <Text textStyle={"h5"} mt={4} fontWeight={"bold"}>
+      <Text
+        textStyle={"h5"}
+        mt={4}
+        fontWeight={"bold"}
+        color={inverted ? "white" : "black"}
+      >
         {text}
       </Text>
     </CustomCard>
