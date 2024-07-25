5fd9ef7be0bb99769fedd418051e690deffdbadd
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-25 20:08:38 +0530

    added text

--- src/pages/Dashboard/components/Transactions.jsx
+++ src/pages/Dashboard/components/Transactions.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import { Text } from "@chakra-ui/react";
+import React from "react";
 
 function Transactions() {
   return (
-    <div>
-      
-    </div>
-  )
+    <CustomCard>
+      <Text>Recent Transactions</Text>
+    </CustomCard>
+  );
 }
 
-export default Transactions
+export default Transactions;
