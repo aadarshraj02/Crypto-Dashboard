c4751f02e9fb9056b41a18cd29c1dab294f8fb37
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-25 23:19:23 +0530

    added bg url and aplied styling on bg image

--- src/pages/Dashboard/components/InfoCard.jsx
+++ src/pages/Dashboard/components/InfoCard.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { CustomCard } from "../../../chakra/CustomCard";
 import { Tag } from "@chakra-ui/react";
 
-function InfoCard() {
+function InfoCard({ imgUrl }) {
   return (
-    <CustomCard>
+    <CustomCard bgImage={imgUrl} bgSize={"cover"} bgRepeat={"no-repeat"}>
       <Tag bg="p.purple" color={"white"} borderRadius={"full"}>
         {" "}
         Loan
