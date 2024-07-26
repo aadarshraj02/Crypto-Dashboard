5626cf2878f02bd1b16b565bc774f91c45407b14
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-26 11:50:42 +0530

    added icon

--- src/pages/Support/components/SupportCard.jsx
+++ src/pages/Support/components/SupportCard.jsx
@@ -1,7 +1,15 @@
+import { Flex, Icon, Stack } from "@chakra-ui/react";
 import React from "react";
+import { IoMdMail } from "react-icons/io";
 
 function SupportCard() {
-  return <div></div>;
+  return (
+    <Flex>
+      <Stack>
+        <Icon as={IoMdMail}></Icon>
+      </Stack>
+    </Flex>
+  );
 }
 
 export default SupportCard;
