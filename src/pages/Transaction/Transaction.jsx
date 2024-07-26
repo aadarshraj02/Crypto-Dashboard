480c84ba4d9ccea8fe12159ea64339e85d1bff78
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-26 08:51:04 +0530

    added flex and a butoon

--- src/pages/Transaction/Transaction.jsx
+++ src/pages/Transaction/Transaction.jsx
@@ -1,10 +1,17 @@
-import { Card } from "@chakra-ui/react";
+import { Button, Card, Flex, Icon } from "@chakra-ui/react";
 import React from "react";
 import DashboardLayout from "../../components/DashboardLayout";
+import { FaArrowDown } from "react-icons/fa6";
 
 function Transaction() {
   return (
     <DashboardLayout>
+      <Flex justify={"end"} mt={6} mb={3}>
+        <Button>
+          {" "}
+          <Icon as={FaArrowDown} mr={2}></Icon> Export Csv
+        </Button>
+      </Flex>
       <Card></Card>
     </DashboardLayout>
   );
