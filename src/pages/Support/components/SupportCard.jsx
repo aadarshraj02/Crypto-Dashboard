1bdae7bd74d57a0d4596c2c9de58a24573808956
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-26 12:55:15 +0530

    wrapped in a stack and applied spacing

--- src/pages/Support/components/SupportCard.jsx
+++ src/pages/Support/components/SupportCard.jsx
@@ -29,33 +29,35 @@ function SupportCard() {
       </Stack>
 
       <Card p={6} borderRadius={"1rem"}>
-        <Text fontWeight={"semibold"} fontSize={"sm"}>
-          You will response within 24 hours of time of submit.
-        </Text>
+        <Stack spacing={3}>
+          <Text fontWeight={"semibold"} fontSize={"sm"}>
+            You will response within 24 hours of time of submit.
+          </Text>
 
-        <HStack>
+          <HStack>
+            <FormControl>
+              <FormLabel>First Name</FormLabel>
+              <Input type="text" placeholder="Enter Your First Name" />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Last Name</FormLabel>
+              <Input type="text" placeholder="Enter Your Last Name" />
+            </FormControl>
+          </HStack>
           <FormControl>
-            <FormLabel>First Name</FormLabel>
-            <Input type="text" placeholder="Enter Your First Name" />
+            <FormLabel>Email</FormLabel>
+            <Input type="email" placeholder="Enter Your Email" />
           </FormControl>
           <FormControl>
-            <FormLabel>Last Name</FormLabel>
-            <Input type="text" placeholder="Enter Your Last Name" />
+            <FormLabel>Message</FormLabel>
+            <Textarea type="text" placeholder="Enter Your Message" />
           </FormControl>
-        </HStack>
-        <FormControl>
-          <FormLabel>Email</FormLabel>
-          <Input type="email" placeholder="Enter Your Email" />
-        </FormControl>
-        <FormControl>
-          <FormLabel>Message</FormLabel>
-          <Textarea type="text" placeholder="Enter Your Message" />
-        </FormControl>
-        <Checkbox defaultChecked>I agree with Terms & Conditions</Checkbox>
+          <Checkbox defaultChecked>I agree with Terms & Conditions</Checkbox>
 
-        <Stack>
-          <Button>Send a Message</Button>
-          <Button colorScheme="gray">Book a Meeting</Button>
+          <Stack>
+            <Button>Send a Message</Button>
+            <Button colorScheme="gray">Book a Meeting</Button>
+          </Stack>
         </Stack>
       </Card>
     </Flex>
