85210e7336a84727d53162d9cdf1ecc8456c1a11
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-26 13:02:46 +0530

    moved in contact card component

--- src/pages/Support/components/SupportCard.jsx
+++ src/pages/Support/components/SupportCard.jsx
@@ -34,43 +34,7 @@ function SupportCard() {
         </Text>
       </Stack>
 
-      <Card p={6} borderRadius={"1rem"} flexGrow={1}>
-        <Stack spacing={3}>
-          <Text fontWeight={"semibold"} fontSize={"sm"}>
-            You will response within 24 hours of time of submit.
-          </Text>
-
-          <HStack
-            flexDirection={{
-              base: "column",
-              md: "row",
-            }}
-          >
-            <FormControl>
-              <FormLabel>First Name</FormLabel>
-              <Input type="text" placeholder="Enter Your First Name" />
-            </FormControl>
-            <FormControl>
-              <FormLabel>Last Name</FormLabel>
-              <Input type="text" placeholder="Enter Your Last Name" />
-            </FormControl>
-          </HStack>
-          <FormControl>
-            <FormLabel>Email</FormLabel>
-            <Input type="email" placeholder="Enter Your Email" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>Message</FormLabel>
-            <Textarea type="text" placeholder="Enter Your Message" />
-          </FormControl>
-          <Checkbox defaultChecked>I agree with Terms & Conditions</Checkbox>
-
-          <Stack>
-            <Button>Send a Message</Button>
-            <Button colorScheme="gray">Book a Meeting</Button>
-          </Stack>
-        </Stack>
-      </Card>
+    
     </Flex>
   );
 }
