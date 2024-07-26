7c7a2db441ceef75381c50307c5dc58391478ad5
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-26 08:20:57 +0530

    added path and elements for react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import Dashboard from "./pages/Dashboard/Dashboard";
+import Transaction from "./pages/Transaction/Transaction";
+import Support from "./pages/Support/Support";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+  },
+  {
+    path: "/transactions",
+    element: <Transaction />,
+  },
+  {
+    path: "/support",
+    element: <Support />,
+  },
+]);
 
 function App() {
   return (
     <>
-      <Dashboard />
+      <RouterProvider router={router} />
     </>
   );
 }
