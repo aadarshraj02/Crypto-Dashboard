3a17d68a5a292ae697c6fa42f5d6951264b90904
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-26 08:58:09 +0530

    added tabs layout from chakra ui

--- src/pages/Transaction/Transaction.jsx
+++ src/pages/Transaction/Transaction.jsx
@@ -1,7 +1,8 @@
-import { Button, Card, Flex, Icon } from "@chakra-ui/react";
+import { Button, Card, Flex, Icon, TabIndicator } from "@chakra-ui/react";
 import React from "react";
 import DashboardLayout from "../../components/DashboardLayout";
 import { FaArrowDown } from "react-icons/fa6";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
 
 function Transaction() {
   return (
@@ -12,7 +13,32 @@ function Transaction() {
           <Icon as={FaArrowDown} mr={2}></Icon> Export Csv
         </Button>
       </Flex>
-      <Card></Card>
+      <Card>
+        <Tabs position="relative" variant="unstyled">
+          <TabList>
+            <Tab>One</Tab>
+            <Tab>Two</Tab>
+            <Tab>Three</Tab>
+          </TabList>
+          <TabIndicator
+            mt="-1.5px"
+            height="2px"
+            bg="blue.500"
+            borderRadius="1px"
+          />
+          <TabPanels>
+            <TabPanel>
+              <p>one!</p>
+            </TabPanel>
+            <TabPanel>
+              <p>two!</p>
+            </TabPanel>
+            <TabPanel>
+              <p>three!</p>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Card>
     </DashboardLayout>
   );
 }
