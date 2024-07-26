cf38a0f98d171742db7b9fed83145615824ec7d5
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-26 11:47:23 +0530

    imported and added supportcard

--- src/pages/Support/Support.jsx
+++ src/pages/Support/Support.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import DashboardLayout from "../../components/DashboardLayout";
+import SupportCard from "./components/SupportCard";
 
 function Support() {
-  return <DashboardLayout title={"Support"}></DashboardLayout>;
+  return (
+    <DashboardLayout title={"Support"}>
+      <SupportCard />
+    </DashboardLayout>
+  );
 }
 
 export default Support;
